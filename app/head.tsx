756250e85ae6b4836452042e4971582b46b40853
Jesús Valero <jvalerodev@gmail.com>
2023-01-04 19:16:51 -0400

    added header

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+const Head = () => {
+  return (
+    <>
+      <title>JV News</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+};
+
+export default Head;
