d9fee881217bddce4b3b7c90282a631bfb80211c
Jesús Valero <jvalerodev@gmail.com>
2023-01-05 19:45:34 -0400

    added components to show news

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
-const Home = () => {
-  return <div>Home</div>;
+import { categories } from '../constants';
+import fetchNews from '../utils/fetchNews';
+import NewsList from './NewsList';
+import response from '../response.json';
+import sortNewsByImage from '../utils/sortNewsByImage';
+
+const Home = async () => {
+  let news: NewsResponse = response;
+  news = sortNewsByImage(news);
+  // const news: NewsResponse = await fetchNews(categories.join(','));
+
+  return (
+    <main>
+      <NewsList news={news} />
+    </main>
+  );
 };
 
 export default Home;
