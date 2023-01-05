611d7def97424876652b9ff17a6e5a8aa1ed1fdc
Jesús Valero <jvalerodev@gmail.com>
2023-01-05 19:43:59 -0400

    added new types

--- typings.d.ts
+++ typings.d.ts
@@ -6,3 +6,28 @@ type Category =
   | 'science'
   | 'sports'
   | 'technology';
+
+type Pagination = {
+  count: number;
+  limit: number;
+  offset: number;
+  total: number;
+};
+
+type Article = {
+  author: string | null;
+  category: string;
+  country: string;
+  description: string;
+  image: string | null;
+  language: string;
+  published_at: string;
+  source: string;
+  title: string;
+  url: string;
+};
+
+type NewsResponse = {
+  pagination: Pagination;
+  data: Article[];
+};
